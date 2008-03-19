0fca9208e2cb734f22a88e68aa5c05b78594ee29
djfiander <djfiander>
2008-03-19 01:26:00 +0000

    Add copyright statements

--- Sip/Constants.pm
+++ Sip/Constants.pm
@@ -1,4 +1,22 @@
 #
+# Copyright (C) 2006-2008  Georgia Public Library Service
+# 
+# Author: David J. Fiander
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2 of the GNU General Public
+# License as published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307 USA
+#
 # Sip::Constants.pm
 #
 # Various protocol constant values for 3M's Standard Interchange
